4f4c31f894c1a6ae7b48ccc030cc1f6f1edc1156
Julia Lu Dac <julialudac@yahoo.fr>
2026-06-26 16:25:26 +0200

    Feat: Replace download document behavior by open in new tab

--- components/DocumentIcon.tsx
+++ components/DocumentIcon.tsx
@@ -3,7 +3,7 @@ import { getDocumentUrl } from '@/utils';
 
 export function DocumentIcon({ document }: { document: Upload }) {
   return (
-    <a href={getDocumentUrl(document)} download>
+    <a href={getDocumentUrl(document)} target="_blank" rel="noreferrer noopener">
       <span aria-hidden="true" style={{ display: 'inline-block', width: 24, height: 24, marginRight: 8 }}>
         📄
       </span>
